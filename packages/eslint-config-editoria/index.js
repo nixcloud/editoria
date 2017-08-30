9c51629c171b2226b0428b6f0b17e91333a06658
yannis <yannisbarlas@gmail.com>
2017-08-30 15:37:09 +0300

    add sorting of methods for react components

--- packages/eslint-config-editoria/index.js
+++ packages/eslint-config-editoria/index.js
@@ -5,5 +5,20 @@ module.exports = {
     "airbnb",
     "standard",
     "standard-react"
-  ]
+  ],
+  "react/sort-comp": [1, {
+    "order": [
+      "constructor",
+      "lifecycle",
+      "everything-else",
+      "/^on.+$/",
+      "rendering"
+    ],
+    "groups": {
+      "rendering": [
+        '/^render.+$/',
+        'render'
+      ]
+    }
+  }]
 };
