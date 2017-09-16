3c26577cc4ef49ccc9fdf401d2985cd68e412036
john <johnbarlas39@gmail.com>
2017-09-16 18:19:55 +0300

    add sorting for object keys

--- packages/eslint-config-editoria/index.js
+++ packages/eslint-config-editoria/index.js
@@ -7,7 +7,8 @@ module.exports = {
     "standard-react"
   ],
   "rules": {
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "sort-keys": [1, "asc", { "caseSensitive": false }]
   },
   "react/sort-comp": [1, {
     "order": [
