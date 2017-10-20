5dcd5f3a196da7cbff8cc4a7905f0b1f31444260
Alf Eaton <eaton.alf@gmail.com>
2017-10-20 16:45:48 +0100

    Remove REDUXLOG_OFF

--- packages/editoria-app/webpack/webpack.development.config.js
+++ packages/editoria-app/webpack/webpack.development.config.js
@@ -27,8 +27,7 @@ module.exports = [
       new webpack.NoEmitOnErrorsPlugin(),
       new webpack.DefinePlugin({
         'process.env': {
-          NODE_ENV: JSON.stringify('dev'),
-          REDUXLOG_OFF: process.env.REDUXLOG_OFF
+          NODE_ENV: JSON.stringify('dev')
         }
       }),
       universal.plugins.copy,
