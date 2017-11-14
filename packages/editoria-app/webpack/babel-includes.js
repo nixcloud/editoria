9cf508be080c83e0cbfb43150315d24b0f00ca2c
Alexandros Georgantas <alexgeorg86@gmail.com>
2017-11-14 18:24:13 +0200

    Fix for the app path

--- packages/editoria-app/webpack/babel-includes.js
+++ packages/editoria-app/webpack/babel-includes.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 const babelIncludes = [
   new RegExp('/pubsweet-client/src'),
-  new RegExp(path.join(__dirname, '../app')),
+  path.join(__dirname, '..', 'app'),
   new RegExp('/pubsweet-.*'),
   new RegExp('/editoria-.*'),
   new RegExp('/wax-editor-.*')
