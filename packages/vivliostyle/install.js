f35481851f39db993b086abc18bd1188afc457c8
Alexandros Georgantas <alexgeorg86@gmail.com>
2018-05-24 17:16:40 +0300

    change vars to consts

--- packages/vivliostyle/install.js
+++ packages/vivliostyle/install.js
@@ -1,9 +1,10 @@
 const request = require('request')
-var path = require('path')
+const path = require('path')
 const fs = require('fs')
 const Promise = require('bluebird')
 const execP = Promise.promisify(require('child_process').exec)
-var extract = require('extract-zip')
+const extract = require('extract-zip')
+
 const vivliostyleVersion = 'vivliostyle-js-2017.6'
 const vivliostyleZip = 'vivliostyle.zip'
 const vivliostylePath = 'vivliostyle-viewer'
@@ -13,9 +14,30 @@ const downloadLink = `${viliostyleHost}/downloads/${vivliostyleVersion}.zip`
 if (!fs.existsSync(vivliostylePath)) {
   if (!fs.existsSync(vivliostyleZip)) {
     request(downloadLink)
-    .pipe(fs.createWriteStream('vivliostyle.zip'))
-    .on('finish', async () => {
-      extract(path.normalize(path.join(__dirname, vivliostyleZip)), {dir: path.normalize(__dirname)}, async (err) => {
+      .pipe(fs.createWriteStream('vivliostyle.zip'))
+      .on('finish', async () => {
+        extract(
+          path.normalize(path.join(__dirname, vivliostyleZip)),
+          { dir: path.normalize(__dirname) },
+          async err => {
+            if (err) {
+              console.error(err)
+              return
+            }
+            try {
+              await execP(`mv ${vivliostyleVersion} ${vivliostylePath}`)
+              await execP(`rm ${vivliostyleZip}`)
+            } catch (error) {
+              console.error(error)
+            }
+          },
+        )
+      })
+  } else {
+    extract(
+      path.normalize(path.join(__dirname, vivliostyleZip)),
+      { dir: path.normalize(__dirname) },
+      async err => {
         if (err) {
           console.error(err)
           return
@@ -26,21 +48,7 @@ if (!fs.existsSync(vivliostylePath)) {
         } catch (error) {
           console.error(error)
         }
-        return
-      })
-    })
-  } else {
-  extract(path.normalize(path.join(__dirname, vivliostyleZip)), {dir: path.normalize(__dirname)}, async (err) => {
-      if (err) {
-        console.error(err)
-        return
-      }
-      try {
-        await execP(`mv ${vivliostyleVersion} ${vivliostylePath}`)
-        await execP(`rm ${vivliostyleZip}`)
-      } catch (error) {
-        console.error(error)
-      }
-   })
+      },
+    )
   }
 }
