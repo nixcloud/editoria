263f89be0de6c08e3eb5ed4d6657a8c7f36cefb2
Alexandros Georgantas <alexgeorg86@gmail.com>
2018-06-04 13:47:32 +0300

    logger config added

--- packages/editoria-app/config/development.js
+++ packages/editoria-app/config/development.js
@@ -1,9 +1,28 @@
 const { deferConfig } = require('config/defer')
+const path = require('path')
+const winston = require('winston')
+
+const logger = new winston.Logger({
+  transports: [
+    new winston.transports.Console({
+      colorize: true,
+    }),
+    new winston.transports.File({
+      name: 'info-file',
+      filename: 'app.log',
+      handleExceptions: true,
+      humanReadableUnhandledException: true,
+      dirname: path.join(__dirname, '../logs/dev'),
+      level: 'info',
+    }),
+  ],
+})
 
 module.exports = {
   'pubsweet-server': {
     baseUrl: deferConfig(
       cfg => `http://localhost:${cfg['pubsweet-server'].port}`,
     ),
+    logger,
   },
 }
--- packages/editoria-app/config/production.js
+++ packages/editoria-app/config/production.js
@@ -1,9 +1,27 @@
 const { deferConfig } = require('config/defer')
+const path = require('path')
+const winston = require('winston')
 
+const logger = new winston.Logger({
+  transports: [
+    new winston.transports.Console({
+      colorize: true,
+    }),
+    new winston.transports.File({
+      name: 'info-file',
+      filename: 'app.log',
+      handleExceptions: true,
+      humanReadableUnhandledException: true,
+      dirname: path.join(__dirname, '../logs/production'),
+      level: 'info',
+    }),
+  ],
+})
 module.exports = {
   'pubsweet-server': {
     baseUrl: deferConfig(
       cfg => `http://localhost:${cfg['pubsweet-server'].port}`,
     ),
+    logger,
   },
 }
